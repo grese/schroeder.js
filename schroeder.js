311e8399930216d06f444daacd63f86b49c476af
grese <johngrese@yahoo.com>
2015-05-12 17:42:10 -0700

    updated audio sprite, and added code from jspiano app

--- schroeder.js
+++ schroeder.js
@@ -1,6 +1,9 @@
+export default
 (function(){
     // verify support for WebAudio API...
     var ctx = null;
+    var bufferCache = {};
+
     try {
         window.AudioContext = window.AudioContext || window.webkitAudioContext;
         ctx = new AudioContext();
@@ -9,7 +12,12 @@
     }
 
     var CodecTester = function(){
-        var audio = new Audio();
+        var audio;
+        try{
+            audio = new Audio();
+        }catch(e){
+            console.error('Audio object is not supported in this browser.', e);
+        }
         var codecList = {
             mp3: !!audio.canPlayType('audio/mpeg;').replace(/^no$/, ''),
             opus: !!audio.canPlayType('audio/ogg; codecs="opus"').replace(/^no$/, ''),
@@ -24,9 +32,152 @@
             return (codecList[key] !== undefined) ? codecList[key] : false;
         };
     };
-    var CODECS = new CodecTester();
+    var CODECS = new CodecTester(),
+        FILTER_TYPES = [
+            "lowpass",
+            "highpass",
+            "bandpass",
+            "lowshelf",
+            "highshelf",
+            "peaking",
+            "notch",
+            "allpass"
+        ];
 
-    var bufferCache = {};
+    var BiQuadFilter = function(type, options){
+        options = options || {};
+        this.type = type;
+        this.setOptions(options);
+    };
+    BiQuadFilter.prototype.setOptions = function(options){
+        switch(this.type){
+            case 'lowpass':
+            case 'highpass':
+            case 'bandpass':
+            case 'notch':
+            case 'allpass':
+                this.frequency = options.frequency;
+                this.q = options.q;
+                break;
+            case 'lowshelf':
+            case 'highshelf':
+                this.frequency = options.frequency;
+                this.gain = options.gain;
+                break;
+            case 'peaking':
+                this.frequency = options.frequency;
+                this.q = options.q;
+                this.gain = options.gain;
+                break;
+            default:
+                console.error('Unsupported filter type. Current supported filter types are: ' + FILTER_TYPES.join(', '));
+                return;
+        }
+    };
+
+    var SIMPLE_REVERB_DEFAULT = {
+        name: "SimpleReverb",
+        seconds: {
+            min: 1,
+            max: 50,
+            defaultValue: 3,
+            type: "float"
+        },
+        decay: {
+            min: 0,
+            max: 100,
+            defaultValue: 2,
+            type: "float"
+        },
+        reverse: {
+            min: 0,
+            max: 1,
+            defaultValue: 0,
+            type: "bool"
+        }
+    };
+    /*
+     * Simple Reverb constructor.
+     *
+     * @param {AudioContext} context
+     * @param {object} opts
+     * @param {number} opts.seconds
+     * @param {number} opts.decay
+     * @param {boolean} opts.reverse
+     */
+    function SimpleReverb (context, opts) {
+        this.input = this.output = context.createConvolver();
+        this._context = context;
+
+        var p = SIMPLE_REVERB_DEFAULT;
+        opts = opts || {};
+        this._seconds   = opts.seconds  || p.seconds.defaultValue;
+        this._decay     = opts.decay    || p.decay.defaultValue;
+        this._reverse   = opts.reverse  || p.reverse.defaultValue;
+        this._buildImpulse();
+    }
+
+    SimpleReverb.prototype = Object.create(null, {
+        //AudioNode prototype `connect` method.
+        //@param {AudioNode} dest
+        connect: {
+            value: function (dest) {
+                this.output.connect( dest.input ? dest.input : dest );
+            }
+        },
+        //AudioNode prototype `disconnect` method.
+        disconnect: {
+            value: function () {
+                this.output.disconnect();
+            }
+        },
+        //Utility function for building an impulse response from the module parameters.
+        _buildImpulse: {
+            value: function () {
+                var rate = this._context.sampleRate,
+                    length = rate * this.seconds,
+                    decay = this.decay,
+                    impulse = this._context.createBuffer(2, length, rate),
+                    impulseL = impulse.getChannelData(0),
+                    impulseR = impulse.getChannelData(1),
+                    n, i;
+
+                for (i = 0; i < length; i++) {
+                    n = this.reverse ? length - i : i;
+                    impulseL[i] = (Math.random() * 2 - 1) * Math.pow(1 - n / length, decay);
+                    impulseR[i] = (Math.random() * 2 - 1) * Math.pow(1 - n / length, decay);
+                }
+
+                this.input.buffer = impulse;
+            }
+        },
+        //Module parameter metadata.
+        seconds: {
+            enumerable: true,
+            get: function () { return this._seconds; },
+            set: function (value) {
+                this._seconds = value;
+                this._buildImpulse();
+            }
+        },
+        decay: {
+            enumerable: true,
+            get: function () { return this._decay; },
+            set: function (value) {
+                this._decay = value;
+                this._buildImpulse();
+            }
+        },
+        reverse: {
+            enumerable: true,
+            get: function () { return this._reverse; },
+            set: function (value) {
+                this._reverse = value;
+                this._buildImpulse();
+            }
+        }
+
+    });
 
     /*
      Instrument -
@@ -53,6 +204,7 @@
         this.sprite = options.sprite;
         this.format = options.format;
         this.gain = (options.gain !== undefined) ? options.gain : 1;
+        this.filters = [];
         this._ctx = options.ctx;
         this._audioNode = new Audio();
         this._gainNode = null;
@@ -73,7 +225,7 @@
         this.gain = value;
         this._gainNode.gain.value = this.gain;
     };
-    Instrument.prototype.load = function(cb){
+    Instrument.prototype.load = function(cb, errCb){
         this._url = this.getUrlForCodec(this.format);
         if(!this._url){
             console.error('Could not find valid format ' + this.name + '.');
@@ -88,12 +240,15 @@
             req.responseType = 'arraybuffer';
             req.onload = function() {
                 // Decode asynchronously
-                instr.decode(req.response, cb);
+                instr.decode(req.response, cb, errCb);
+            };
+            req.onerror = function(){
+                if(errCb instanceof Function) { errCb(); }
             };
             req.send();
         }
     };
-    Instrument.prototype.decode = function(arraybuffer, cb){
+    Instrument.prototype.decode = function(arraybuffer, cb, errCb){
         var instr = this;
         this._ctx.decodeAudioData(arraybuffer, function(buffer) {
             instr.duration = Math.ceil(instr._audioNode.duration * 10) / 10;
@@ -108,6 +263,7 @@
         }, function(err){
             // decoding error:
             console.error('An error occurred during decoding ' + instr._url, err);
+            if(errCb instanceof Function) { errCb(); }
         });
     };
     Instrument.prototype.play = function(key, options){
@@ -128,9 +284,9 @@
             this._gainNode.connect(context.destination);
             // Some older browsers use noteOn instead of start...
             if(source.start){
-            	source.start(0, spriteItem.start, duration);
+                source.start(0, spriteItem.start, duration);
             }else{
-            	source.noteOn(0, spriteItem.start, duration);
+                source.noteOn(0, spriteItem.start, duration);
             }
             setTimeout(function(){
                 source.stop();
@@ -157,28 +313,28 @@
     };
     Schroeder.prototype.unlockAudioContext = function(){
         // unlockAudioContext: used for unlocking WebAudio in iOS
-    	// create empty buffer
-    	var buffer = this._ctx.createBuffer(1, 1, 22050);
-    	var source = this._ctx.createBufferSource();
-    	source.buffer = buffer;
-    	// connect to output (your speakers)
-    	source.connect(this._ctx.destination);
-    	// play the empty buffer (some older browsers require noteOn instead of start)...
-    	if(source.start){
-    		source.start(0);
-    	}else{
-    		source.noteOn(0);
-    	}
+        // create empty buffer
+        var buffer = this._ctx.createBuffer(1, 1, 22050);
+        var source = this._ctx.createBufferSource();
+        source.buffer = buffer;
+        // connect to output (your speakers)
+        source.connect(this._ctx.destination);
+        // play the empty buffer (some older browsers require noteOn instead of start)...
+        if(source.start){
+            source.start(0);
+        }else{
+            source.noteOn(0);
+        }
     };
-    Schroeder.prototype.addInstrument = function(options, cb){
+    Schroeder.prototype.addInstrument = function(options, cb, errCb){
         options = options || {};
         var instrument;
-        if(!this._instruments[options.name]){
+        if(!this._instruments[options.id]){
             options.ctx = this._ctx;
             options.format = this._format;
             instrument = new Instrument(options);
-            instrument.load(cb);
-            this._instruments[options.name] = instrument;
+            instrument.load(cb, errCb);
+            this._instruments[options.id] = instrument;
         }else{
             console.error('That sound already seems to exist.');
         }
@@ -209,5 +365,5 @@
         }
     };
 
-    return new Schroeder();
+    return Schroeder;
 })();
